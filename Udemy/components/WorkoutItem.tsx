9167b95e373a816b58dd066ed07b1642d413ced8
ManulThisuraka <it20163754@my.sliit.lk>
2022-03-15 17:40:39 +0530

    final commit

--- Udemy/components/WorkoutItem.tsx
+++ Udemy/components/WorkoutItem.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Workout} from '../types/data';
+import {secToMin} from '../utils/time';
 
 export default function WorkoutItem({item}: {item: Workout}) {
   return (
     <View style={styles.container}>
       <Text style={styles.name}>{item.name}</Text>
-      <Text style={styles.duration}>Duration: {item.duration}</Text>
+      <Text style={styles.duration}>Duration: {secToMin(item.duration)}</Text>
       <Text style={styles.difficulty}>Difficulty: {item.difficulty}</Text>
     </View>
   );
