d3f6696450ca9da46f9eb58d216459ca7a2ecf7e
ManulThisuraka <it20163754@my.sliit.lk>
2022-03-15 15:26:46 +0530

    Testing

---
+++ Udemy/components/WorkoutItem.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Workout} from '../types/data';
+
+export default function WorkoutItem({item}: {item: Workout}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.duration}>Duration: {item.duration}</Text>
+      <Text style={styles.difficulty}>Difficulty: {item.difficulty}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    borderRadius: 10,
+    borderColor: 'rgba(0,0,0,0.1)',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  duration: {
+    fontSize: 15,
+  },
+  difficulty: {
+    fontSize: 15,
+  },
+});
--- Udemy/screens/HomeScreen.tsx
+++ Udemy/screens/HomeScreen.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 import {Text, View, Button, StyleSheet, FlatList} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 import data from '../data.json';
+import {Workout} from '../types/data';
+import WorkoutItem from '../components/WorkoutItem';
 
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
+  const workout: Workout = {
+    slug: 'fsdefsd',
+    name: 'fsdfds',
+    duration: 455,
+    difficulty: 'easy',
+    sequence: [],
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={data}
+        data={data as Workout[]}
+        renderItem={WorkoutItem}
         keyExtractor={item => item.slug}
-        renderItem={({item}) => (
-          <View>
-            <Text>{item.name}</Text>
-          </View>
-        )}
       />
     </View>
   );
